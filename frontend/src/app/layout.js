c156c14f74979b4c16eb607ca8d83a2f989bc949
Amine SGHIR <mp6099@engie.com>
2024-07-17 16:50:59 +0200

    chore: Update metadata in layout.js for PITCHOP website

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -12,20 +12,20 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "Mentoroc",
-  description: "L'assitant IA dédié au mentors OPENCLASSROOMS !",
+  title: "PITCHOP",
+  description: "Website description",
 };
 
 export default function RootLayout({ children }) {
   noStore()
   const envVars = JSON.parse(JSON.stringify(process.env))
   // keep only the variables that start with NEXT_PUBLIC_
-  // Object.keys(envVars).forEach((key) => {
-  //   if (!key.startsWith("_PUBLIC_")) {
-  //     delete envVars[key];
-  //   }
-  // });
-  api.baseUrl = envVars._PUBLIC_API_URL;
+  Object.keys(envVars).forEach((key) => {
+    if (!key.startsWith("NEXT_PUBLIC_")) {
+      delete envVars[key];
+    }
+  });
+  api.baseUrl = envVars.NEXT_PUBLIC_API_URL;
   return (
     <html lang="fr">
       <body className={`min-h-screen antialiased ${poppins.className}`}>
