71a51c254f6b156bcdf9cfe174ada788d2dd300f
Amine SGHIR <mp6099@engie.com>
2024-08-23 13:55:43 +0200

    chore: Update Dockerfile and entrypoint.sh to support different environments

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,17 +2,26 @@
 
 set -e
 
-# if [ "$DATABASE" = "postgres" ]
-# then
-# echo "Waiting for postgres..."
+if [ "$1" = "prod_postgres" ]
+then
+    echo "Waiting for postgres..."
 
-# while ! nc -z $DB_HOST $DB_PORT; do
-#   sleep 0.1
-# done
+    while ! nc -z $DB_HOST $DB_PORT; do
+      sleep 0.1
+    done
 
-# echo "PostgreSQL started"
-# fi
+    echo "PostgreSQL started"
+fi
 
-poetry run python manage.py migrate
-poetry run python manage.py collectstatic --noinput
-poetry run gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+# run if the first argument is "prod" or "prod_postgres"
+if [ "$1" = "prod" ] || [ "$1" = "prod_postgres" ]
+then
+    poetry run python manage.py migrate
+    poetry run python manage.py collectstatic --noinput
+    poetry run gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+fi
+
+if [ "$1" = "dev" ]
+then
+    poetry run python manage.py runserver 0.0.0.0:8000
+fi
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-  },
+  theme: {},
   daisyui: {
-    themes: ['pastel']
+    themes: ["light"],
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [require("daisyui")],
 };
