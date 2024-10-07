6ecd3b3b9c85c56aed806a1d86ac5fb55d45b28f
Amine SGHIR <mp6099@engie.com>
2024-10-07 23:14:18 +0200

    Refactor entrypoint.sh to bind gunicorn to 0.0.0.0:8000 for production server

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,7 +7,7 @@ then
     poetry run python manage.py migrate
     poetry run python manage.py collectstatic --noinput
     poetry run python manage.py crontab add
-    poetry run gunicorn backend.asgi:application --host 0.0.0.0 --port 8000 --workers 4 --worker-class uvicorn.workers.UvicornWorker
+    poetry run gunicorn backend.asgi:application --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
 fi
 
 if [ "$1" = "dev" ]
