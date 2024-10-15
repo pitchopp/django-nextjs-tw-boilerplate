98a8aacf21d94b78553860c027c8234b29e77531
Amine SGHIR <mp6099@engie.com>
2024-10-15 23:27:23 +0200

    Refactor DashboardLayout component to remove unused imports and simplify authentication check

--- frontend/src/app/dashboard/layout.js
+++ frontend/src/app/dashboard/layout.js
@@ -1,7 +1,5 @@
-"use client";
-import { useEffect } from "react";
 import { FaHome, FaDatabase } from "react-icons/fa";
-import { usePathname, useRouter } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
 import { isAuthenticated, isStaff } from "@/lib/auth";
 import { env } from "next-runtime-env";
 import Sidebar from "@/components/navigation/sidebar";
@@ -25,19 +23,11 @@ const adminNavItems = [
 
 
 export default function DashboardLayout({ children }) {
-  const router = useRouter();
   const pathname = usePathname();
-  const authenticated = isAuthenticated();
 
   const allNavItems = isStaff() ? navItems.concat(adminNavItems) : navItems;
 
-  useEffect(() => {
-    if (router && !authenticated) router.push(`/auth/login?next=${pathname}`);
-  }, [router, authenticated, pathname]);
-
-  if (!authenticated) {
-    return <div>Vous devez être connecté pour accéder à cette page.</div>;
-  }
+  if (!isAuthenticated()) redirect(`/auth/login?next=${pathname}`);
 
   return (
     <main>
