be03d273efe4eaa55a24657c2d0354f9b67958d4
Amine SGHIR <mp6099@engie.com>
2024-10-16 17:36:10 +0200

    Refactor GoogleButton component to accept additional props and wrap in a div container

--- frontend/src/app/auth/login/GoogleButton.js
+++ frontend/src/app/auth/login/GoogleButton.js
@@ -6,7 +6,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
-export default function GoogleButton({ clientId }) {
+export default function GoogleButton({ clientId, className, ...props }) {
   const [loggedIn, setLoggedIn] = useState(false);
 
   const router = useRouter();
@@ -17,7 +17,7 @@ export default function GoogleButton({ clientId }) {
     if (router && loggedIn) router.push(next || "/dashboard");
   }, [router, loggedIn, next]);
 
-  const onLoginSuccess = ({credential}) => {
+  const onLoginSuccess = ({ credential }) => {
     api
       .post("/auth/google/", { credential })
       .then((res) => {
@@ -29,15 +29,17 @@ export default function GoogleButton({ clientId }) {
       });
   };
   return (
-    <GoogleOAuthProvider clientId={clientId}>
-      <GoogleLogin
-        onSuccess={onLoginSuccess}
-        onError={() => {
-          toast.error("Login Failed");
-        }}
-        // useOneTap
-        auto_select
-      />
-    </GoogleOAuthProvider>
+    <div className={`grid justify-center gap-2 ${className}`} {...props}>
+      <GoogleOAuthProvider clientId={clientId}>
+        <GoogleLogin
+          onSuccess={onLoginSuccess}
+          onError={() => {
+            toast.error("Login Failed");
+          }}
+          // useOneTap
+          auto_select
+        />
+      </GoogleOAuthProvider>
+    </div>
   );
 }
