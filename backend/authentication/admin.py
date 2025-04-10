ffbceec80b228371fccc22a8d8863896738c5416
Amine SGHIR <mp6099@engie.com>
2025-04-10 15:39:46 +0200

    Unregister unused models from Django admin for cleaner interface

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth import get_user_model
 from import_export.admin import ImportExportModelAdmin
+from rest_framework.authtoken.models import TokenProxy
+from django.contrib.auth.models import Group
+from django_celery_beat.models import (
+    ClockedSchedule,
+    PeriodicTask,
+    IntervalSchedule,
+    CrontabSchedule,
+    SolarSchedule,
+)
+
+admin.site.unregister(TokenProxy)
+admin.site.unregister(Group)
+admin.site.unregister(ClockedSchedule)
+admin.site.unregister(CrontabSchedule)
+admin.site.unregister(IntervalSchedule)
+admin.site.unregister(PeriodicTask)
+admin.site.unregister(SolarSchedule)
 
 User = get_user_model()
 
